eed0ec6b1af709cd9d90bbc072895d334eb1737d
tamashiro-syuta <syuta5825@gmail.com>
2022-12-18 18:30:52 +0900

    ✅ action mailerのテスト例を記述

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "welcome" do
+    email = UserMailer.with(to: "sample@example.com", name: "sample").welcome
+    # メールが送信キューに保存されるかテスト
+    assert_equal ["from@example.com"], email.from
+    assert_equal ["sample@example.com"], email.to
+    assert_equal "登録完了", email.subject
+    assert_includes email.text_part.decoded, "sample"
+    assert_includes email.html_part.decoded, "sample"
+  end
 end
