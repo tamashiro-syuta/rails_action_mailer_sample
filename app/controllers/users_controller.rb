63f1045bd31c7713678f2989059a351ad97b0b00
tamashiro-syuta <syuta5825@gmail.com>
2022-12-18 18:22:35 +0900

    ✏️ typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
       if @user.save
         # Active Jobによる非同期実行が内部で動いている(deliver_nowもActiveJobのメソッド)
         # ActiveJobと同じようにdeliver_laterメソッドやwaitメソッドなどが使える
-        UserMailer.with(to: @user.email, name: @user.name).welcome.delever_now
+        UserMailer.with(to: @user.email, name: @user.name).welcome.deliver_now
         format.html { redirect_to user_url(@user), notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
